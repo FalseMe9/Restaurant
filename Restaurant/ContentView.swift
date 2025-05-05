f0b3a9196926504912370d73ff10dd8bc12306ed
Billie <150131870+FalseMe9@users.noreply.github.com>
2025-05-05 09:22:45 +0700

    Tab View & Icon

--- Restaurant/ContentView.swift
+++ Restaurant/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView{
-            Tab("", systemImage: "photo"){
+            Tab("", image: "Restaurant"){
                 RestaurantView()
             }
-            Tab("", systemImage: "photo"){
+            Tab("", systemImage: "person.3.fill"){
                 ContactView()
             }
         }
